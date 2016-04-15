265a85e6cb7b630edc5f9475ac536b91bc57ab33
Ivan Flannigan <yoshiboo123@live.com>
2016-04-15 18:21:51 -0500

    Finished the IF macro.

--- Programming Languages/macro-examples.clj
+++ Programming Languages/macro-examples.clj
@@ -23,4 +23,29 @@
      (printf "debug: %s => %s%n" (quote ~expr) result#)
      result#))
 
+(defn keep-between
+  "Returns a new list with all the elements between start
+  and end from the given lst."
+  [start end lst]
+  (take-while #(not= end %)
+    (rest (drop-while #(not= start %) lst))))
+
+(defmacro IF
+  "Provides a conditional statement that is syntactically a
+  bit more similar to those found in languages like Pascal
+  or Fortran. It has the following form:
+  (IF condition :THEN exp1 exp2 … :ELSE exp3 exp4 …)
+  Everything is optional, except the condition. Also, the
+  :ELSE keyword may come before the :THEN keyword."
+  [condition & all]
+  (let [then-part (keep-between :THEN :ELSE all)
+        else-part (keep-between :ELSE :THEN all)]
+    `(if ~condition (do ~@then-part) (do ~@else-part))))
+
 (println (inc (debug (* 4 5))))
+(println (IF (> 3 1) :THEN 'ok :ELSE 'oops))
+
+(println
+  (macroexpand-1 '(IF (> 3 1) 
+                    :ELSE (println "Else section.") 'oops 
+                    :THEN (println "Then section.") 'ok)))
